208f122581e7967af60d7a466a634b3f012d6c81
DASitby <davidsooley551@gmail.com>
2023-03-23 21:32:04 -0230

    add danger, disabled, and onclick props

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,9 +5,12 @@ import "components/Button.scss";
 export default function Button(props) {
    let buttonClass = "button";
 
-  if (props.confirm) {
+   if (props.confirm) {
     buttonClass += " button--confirm";
   }
+  if (props.danger) {
+   buttonClass += " button--danger";
+ }   
 
-  return <button className={buttonClass}>{props.children}</button>;
+  return <button disabled={props.disabled} className={buttonClass} onClick={props.onClick}>{props.children}</button>;
 }
