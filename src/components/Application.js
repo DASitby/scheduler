786f698d78f130bbb4d148488db28bed961b86bb
DASitby <davidsooley551@gmail.com>
2023-03-25 16:24:28 -0230

    refactor all lists to take Controlled List-style prop names

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import DayList from "./DayList";
 import "components/Application.scss";
+import InterviewerList from "./InterviewerList";
 
 const days = [
   {
@@ -19,9 +20,16 @@ const days = [
     spots: 0,
   },
 ];
-
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
 export default function Application(props) {
   const [day, setDay] = useState('Monday')
+  const [interviewer, setInterviewer] = useState('Sylvia Palmer')
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,7 +42,8 @@ export default function Application(props) {
             />
             <hr className="sidebar__separator sidebar--centered" />
             <nav className="sidebar__menu">
-            <DayList days={days} day={day} setDay={setDay}/>
+            <DayList days={days} value={day} onChange={setDay}/>
+            <InterviewerList interviewers ={interviewers} interviewer={interviewer} setInterviewer={setInterviewer}/>
             </nav>
             <img
               className="sidebar__lhl sidebar--centered"
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -9,8 +9,8 @@ const days = props.days.map(day => {
         key={day.id}
         name={day.name} 
         spots={day.spots} 
-        selected={day.name === props.day}
-        setDay={props.setDay}  
+        selected={day.name === props.value}
+        setDay={props.onChange}  
       />
   )})
   return(<ul>
