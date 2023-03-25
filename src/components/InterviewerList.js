b6dd8e8b0f7ef095a8f698047325dce724689aaa
DASitby <davidsooley551@gmail.com>
2023-03-25 15:49:31 -0230

    add .map function to render a ListItem for each Interviewer

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,22 @@
 import React from "react";
 import 'components/InterviewerList.scss'
+import InterviewerListItem from "./InterviewerListItem";
 export default function InterviewerList(props){
+  const interviewers = props.interviewers.map( person => {
+  return ( 
+  <>
+  <InterviewerListItem 
+      key = {person.id}
+      name = {person.name}
+      avatar = {person.avatar}
+      selected = {person.id === props.interviewer}
+      setInterviewer = {props.setInterviewer}
+    />
+    </>
+)})
+
   return (<section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
-    <ul className="interviewers__list"></ul>
+    <ul className="interviewers__list">{interviewers}</ul>
     </section>)
 }
