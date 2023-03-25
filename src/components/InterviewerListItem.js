05defa934f811ea0bd1e6b926d74abee7b6c8e61
DASitby <davidsooley551@gmail.com>
2023-03-25 14:43:12 -0230

    import classNames and set up conditional therein for interviewers_item--selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,9 +1,8 @@
 import React from "react";
-
+import classNames from "classnames";
 export default function InterviewerListItem(props){
-  const interviewerClass = classnames("interviewers__item",{"interviewers__item--selected": props.selected})
-
-  return(<li className="" onClick={() => {props.setInterviewer(props.id)}}>
+  const interviewerClass = classNames("interviewers__item",{"interviewers__item--selected": props.selected})
+  return(<li className={interviewerClass} onClick={() => {props.setInterviewer(props.id)}}>
     <img
       className="interviewers__item-image"
       src={props.avatar}
