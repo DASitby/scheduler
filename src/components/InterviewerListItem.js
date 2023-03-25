14602e4ca1f112081eb49787d76602800bd73b79
DASitby <davidsooley551@gmail.com>
2023-03-25 14:55:04 -0230

    import css and add conditional to only render name when selected

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classNames from "classnames";
+import 'components/InterviewerListItem.scss'
 export default function InterviewerListItem(props){
   const interviewerClass = classNames("interviewers__item",{"interviewers__item--selected": props.selected})
   return(<li className={interviewerClass} onClick={() => {props.setInterviewer(props.id)}}>
@@ -8,6 +9,6 @@ export default function InterviewerListItem(props){
       src={props.avatar}
       alt={props.name}
     />
-    {props.name}
+    {props.selected && props.name}
   </li>)
 }
