8f7f7d041436a83340c4c928b06fa1f318a6a356
DASitby <davidsooley551@gmail.com>
2023-03-25 12:14:13 -0230

    add formatSpots logic to match output to design

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,10 +7,22 @@ export default function DayListItem(props) {
     "day-list__item--selected": props.selected === true,
     "day-list__item--full": props.spots === 0
   });
+  const formatSpots = (value) => {
+  let output = value
+    if (value === 0) {
+    output = "no spots"
+  } else if (value === 1){
+    output = value + ' spot'
+  } else {
+    output = value + ' spots'
+  }
+  return output
+}
+
   return (
     <li className={itemClass} onClick={()=>props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)} remaining</h3>
     </li>
   );
 }
