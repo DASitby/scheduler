55d220a88a8302cb9594fee2f42d4e11dac42153
DASitby <davidsooley551@gmail.com>
2023-03-29 21:28:56 -0230

    add appointment stories and update imports

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Fragment} from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -12,12 +12,12 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
-import Empty from "components/Appointment/Empty"
-import Show from "components/Appointment/Show"
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
-import Status from "components/Appointment/Status"
+import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
-import Form from "components/Appointment/Form"
+import Form from "components/Appointment/Form";
 
 storiesOf("Button", module)
   .addParameters({
@@ -151,6 +151,23 @@ storiesOf("Appointment", module)
   .add("Confirm", () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
   .add("Status", ()=> <Status message={"Deleting"}/>)
   .add("Error",()=><Error message={"Could not delete function"} onClose={action("onClose")}/>)
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  
 
 storiesOf("Form", module)
   .addParameters({
