3ea8712b647433654b09f9d7f3471cd46bc58b10
DASitby <davidsooley551@gmail.com>
2023-03-31 22:42:26 -0230

    import custom hook and assemble new transition variables to write transitions, write first transition from EMPTY to CREATE

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,14 +1,35 @@
 import React, { Fragment } from "react"
+import useVisualMode from "hooks/useVisualMode"
 import Empty from "./Empty"
 import Header from "./Header"
 import Show from "./Show"
+import Form from "./Form"
 import "./styles.scss"
+
 
 export default function Appointment(props) {
   const interview= props.interview
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return( <Fragment>
   <Header time={props.time}/>
-  {props.interview ? <Show student={interview.student} interviewer={interview.interviewer}/> : <Empty/>}
+  {mode === EMPTY && <Empty onAdd={event => transition(CREATE)} />}
+  {mode === SHOW && (
+  <Show
+    student={props.interview.student}
+    interviewer={props.interview.interviewer}
+  />)}
+  {mode === CREATE && (
+    <Form 
+
+    />
+  )}
   </Fragment>
   )
 }
