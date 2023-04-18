adadc07bbe2aebfee8d8b717b7064013b93b86a9
DASitby <davidsooley551@gmail.com>
2023-04-18 14:19:27 -0230

    refactor and add .contains statements to validate steps

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,18 +1,21 @@
-describe("Appointment", () => {
-  it("should book an interview", () => {
-    //Navigate to root
-    cy.visit("/");
-    cy.contains("Monday");
-    //Click Add button
-    cy.get("[alt=Add]").first().click();
-    //Enter their name
-    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-    //Select an interviewer
-    cy.get("[alt='Sylvia Palmer']").click();
-    //Click save
-    cy.contains("Save").click();
-    //Reset database when done
-    cy.request("GET", "/api/debug/reset")
+describe("Appointments", () => {
+  beforeEach(() => {
+   cy.request("GET", "/api/debug/reset");
+   cy.visit("/");
+   cy.contains("Monday");
   });
-  
-});
+ 
+  it("should book an interview", () => {
+  //Click the add button
+    cy.get("[alt=Add]").first().click();
+  //Enter name and select interviewer
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get('[alt="Sylvia Palmer"]').click();
+  //Click Save
+    cy.contains("Save").click();
+  //Check that the appointment is booked
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+ });
+ 
