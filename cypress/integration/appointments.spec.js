7280b7de89012ec9b7bbe31e4c1e78323cd79a7a
DASitby <davidsooley551@gmail.com>
2023-04-18 14:36:47 -0230

    add test for editing appointments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -17,5 +17,19 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    //Click the edit button
+      cy.get("[alt=Edit]").first().invoke('show').click();
+    //Select new interviewer
+      cy.get("[data-testid=student-name-input]").clear().type("Lydia Miller-Jones");      ;
+      cy.get('[alt="Tori Malcolm"]').click();
+    //Click Save
+      cy.contains("Save").click();
+    //Check that the appointment is booked
+      cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+      cy.contains(".appointment__card--show", "Tori Malcolm");
+    });
+
  });
  
